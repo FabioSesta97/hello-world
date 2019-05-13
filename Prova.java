0e383aa5fcdaa2101656caa38ea4a184541e44e3
fabiosesta <sestafabio@gmail.com>
2019-05-13 22:33:16 +0200

    Added constructor and getter/setter for properties "nome" and "cognome".

--- Prova.java
+++ Prova.java
@@ -5,4 +5,25 @@ public class Prova {
 	private String nome;
 	private String cognome;
 
+	public Prova(String nome, String cognome){
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+
+	private String getNome(){
+		return this.nome;
+	}
+
+	private void setNome(String nome){
+		this.nome = nome;
+	}
+
+	private String getCognome(){
+		this.cognome = cognome;
+	}
+
+	private void setCognome(String cognome){
+		this.cognome = cognome;
+	}
+
 }
